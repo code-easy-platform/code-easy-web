40dbaf7d063237ea8ed05f3d348114ea349a4d17
Lucas DeVargas Souza <ls18017@gmail.com>
2019-09-24 22:10:22 -0300

    Iniciado o componente de "ComponentsBar".

--- src/app/pages/editor/tabs/editor-tab/components/components-bar/ComponentsBar.tsx
+++ src/app/pages/editor/tabs/editor-tab/components/components-bar/ComponentsBar.tsx
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 
 export default class ComponentsBar extends Component {
     render() {
-        return(
-            <div>Components bar</div>
+        return (
+            {/* <div cdkDropList id="toolList" [cdkDropListData] = "listTools" class="draggable-list" cdkDropListSortingDisabled
+        cdkDropListConnectedTo = "flowList" >
+            <div class="draggable-item" * ngFor="let tool of listTools" cdkDrag >
+                <mat-icon aria-hidden="false" aria-label="Example home icon">{{ tool?.previewIcon }}</mat-icon>
+                <p class="align-start">{{ tool?.type }}</p>
+          </div >
+        </div > */}
+
         );
     }
 }
