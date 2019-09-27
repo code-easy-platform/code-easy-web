2b0fcdaca2e65d1f33cbc43cf7775c380a39601a
Lucas de Vargas Souza <lvargas@gvdasa.local>
2019-09-27 18:50:20 -0300

    Adicionado o context no app!

--- src/app/pages/editor/tabs/editor-tab/components/code-editor/CodeEditor.tsx
+++ src/app/pages/editor/tabs/editor-tab/components/code-editor/CodeEditor.tsx
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
+import Message from '../../../../../../shared/context/Context';
 
 export default class CodeEditor extends Component {
+    state = {
+        message: 'Minha',
+        toggle: ()=>{this.setState({message: 'teste'});}, // Não funciona
+    }
+
     render() {
-        return(
-            <div>Code editor</div>
+        return (
+            <>
+                <Message.Provider value={this.state}>
+
+
+                    <Message.Consumer>
+                        {({ message, toggle }) => (<Msg message={message} toggle={toggle} />)}
+                    </Message.Consumer>
+
+
+                </Message.Provider>
+
+            </>
         );
     }
 }
+
+const Msg = (props: any) => {
+    return (
+        <>
+            <button onClick={() => props.toggle}>Teste</button>
+            <div>{props.message}</div>
+        </>
+    );
+}
+
---
+++ src/app/shared/context/Context.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default React.createContext({
+    message: 'Minha msg',
+    toggle: () => {}
+});
+
