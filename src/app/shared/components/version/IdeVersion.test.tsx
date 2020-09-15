4ed9b810fe8c47b2281ed5aebd2b6f27eaa1d8a4
lvsouza <ls18017@gmail.com>
2020-09-15 18:51:43 -0300

    test: added tests configurations and added tests to IdeVersion component

---
+++ src/app/shared/components/version/IdeVersion.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { IdeVersion } from './IdeVersion';
+
+test('Get by version code', () => {
+    const { getByText, queryByText, ...rest } = render(<IdeVersion prefix="0.0.1" />);
+    const element = queryByText('0.0.1');
+    expect(element).toBeTruthy();
+});
