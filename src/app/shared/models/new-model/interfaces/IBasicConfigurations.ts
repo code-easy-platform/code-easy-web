17360697c1c8b4368e8398f7c1c6b564eebc2068
lvsouza <ls18017@gmail.com>
2020-09-16 20:49:47 -0300

    feat: added basicconfigurations models and interface

---
+++ src/app/shared/models/new-model/interfaces/IBasicConfigurations.ts
@@ -0,0 +1,80 @@
+import { IProperty } from "../../../components/properties-editor";
+import { EItemType } from "../../../components/flow-editor";
+import { EComponentType } from "../../../enuns";
+import { ITreeItem } from "../../../components/tree-manager";
+
+/**
+ * Common fields between the different project structures
+ */
+export interface IBasicConfigurations<TYPE = EComponentType | EItemType> {
+    /**
+     * Used to visually represent the record
+     */
+    icon?: any;
+    /**
+     * Used for tree it helps to know if the item is a folder or a file
+     */
+    type: TYPE;
+    /**
+     * Used to name a record visually only
+     */
+    label: string;
+    /**
+     * Used to control the ordering of items
+     */
+    ordem?: number;
+    /**
+     * Indicates if the item has an error
+     */
+    hasError?: boolean;
+    /**
+     * Field used to store criation date
+     */
+    createdDate?: Date;
+    /**
+     * Field used to store updated date
+     */
+    updatedDate?: Date;
+    /** 
+     * Used to be able to tell the item flow which items in a tree are currently being edited
+     */
+    isEditing?: boolean;
+    /**
+     * Indicates whether the item has any type of warning
+     */
+    hasWarning?: boolean;
+    /**
+     * Used to describe some more specific detail
+     */
+    description?: string;
+    /** 
+     * Indicates where the item is selected in the tree.
+     */
+    isSelected?: boolean;
+    /**
+     * Used on the tree, it helps to know if the item is expanded or collapsed
+     */
+    isExpanded?: boolean;
+    /**
+     * Used to identify a record more simply.
+     * 
+     *  * Can't have space
+     *  * Cannot have special characters
+     *  * It cannot be empty
+     */
+    readonly name: string;
+    /**
+     * Uuid used as identifier
+     * 
+     * If ***undefined*** the record is being created
+     */
+    id: string | undefined;
+    /**
+     * Used to list all properties of an item
+     */
+    properties?: IProperty[];
+    /**
+     * Used to list all problems in this component
+     */
+    readonly problems: ITreeItem[];
+}
---
+++ src/app/shared/models/new-model/models/BasicConfigurations.ts
@@ -0,0 +1,144 @@
+import { IconError, IconWarning, Utils } from "code-easy-components";
+
+import { IProperty, TypeOfValues } from "../../../components/properties-editor";
+import { ITreeItem } from "../../../components/tree-manager";
+import { IBasicConfigurations } from "../interfaces";
+import { PropertieTypes } from "../../../enuns";
+
+
+type OmitInConstructor = 'name';
+
+export class BasicConfigurations<TYPE> implements IBasicConfigurations<TYPE> {
+
+    private _id: string | undefined = undefined;
+    public get id(): string | undefined {
+        return this._id || Utils.getUUID();
+    };
+    public set id(uuid: string | undefined) {
+        this._id = uuid || Utils.getUUID();
+    };
+
+    public get name(): string {
+        return Utils.getNormalizedString(this.label);
+    };
+
+    public get label(): string {
+        return this._properties.find(prop => prop.propertieType === PropertieTypes.label)?.value || '';
+    };
+    public set label(value: string) {
+        let prop = this._properties?.find(prop => prop.propertieType === PropertieTypes.label);
+        if (prop) {
+            prop.value = value;
+        } else {
+            this._properties.push({
+                value,
+                id: Utils.getUUID(),
+                focusOnRender: true,
+                type: TypeOfValues.string,
+                name: PropertieTypes.label,
+            });
+        }
+    };
+
+    public get description(): string {
+        return this._properties.find(prop => prop.propertieType === PropertieTypes.description)?.value || '';
+    };
+    public set description(value: string) {
+        let prop = this._properties?.find(prop => prop.propertieType === PropertieTypes.description);
+        if (prop) {
+            prop.value = value;
+        } else {
+            this._properties.push({
+                value,
+                id: Utils.getUUID(),
+                name: PropertieTypes.label,
+                type: TypeOfValues.bigstring,
+            });
+        }
+    };
+
+    public get problems(): ITreeItem[] {
+        let problems: ITreeItem[] = [];
+        this.hasWarning = false;
+        this.hasError = false;
+
+        const addProblem = (label: string, type: 'warning' | 'error') => {
+            problems.push({
+                icon: type === 'warning' ? IconWarning : IconError,
+                isDisabledSelect: true,
+                nodeExpanded: false,
+                isSelected: false,
+                id: undefined,
+                iconSize: 15,
+                type: "ITEM",
+                label,
+            });
+        };
+
+        // Valida o label
+        const label = this.label;
+        if (label === '') {
+            addProblem('Field Label cannot be empty', 'error');
+            this._properties
+                .filter(prop => prop.propertieType === PropertieTypes.label)
+                .forEach(prop => { prop.valueHasWarning = false; prop.valueHasError = true });
+            this.hasError = true;
+        } else if (label.length < 3) {
+            addProblem(`Field Label cannot be less than 3 characters in "${label}"`, 'error');
+            this._properties
+                .filter(prop => prop.propertieType === PropertieTypes.label)
+                .forEach(prop => { prop.valueHasWarning = false; prop.valueHasError = true });
+            this.hasError = true;
+        } else if (label.length > 50) {
+            addProblem(`More than 50 characters are not recommended in the item "${label}"`, 'warning');
+            this._properties
+                .filter(prop => prop.propertieType === PropertieTypes.label)
+                .forEach(prop => { prop.valueHasWarning = true; prop.valueHasError = false });
+            this.hasWarning = true;
+        } else {
+            this.properties
+                .filter(prop => prop.propertieType === PropertieTypes.label)
+                .forEach(prop => { prop.valueHasWarning = false; prop.valueHasError = false });
+        }
+
+        return problems;
+    }
+
+    private _properties: IProperty[] = [];
+    public get properties(): IProperty[] { return this._properties };
+    public set properties(props: IProperty[]) {
+
+        // Remove auto focus
+        props.forEach(prop => prop.focusOnRender = false);
+
+        this._properties = props;
+    }
+
+    public icon: any;
+    public type: TYPE;
+    public ordem?: number;
+    public isExpanded?: boolean;
+    public hasError: boolean = false;
+    public isEditing: boolean = false;
+    public isSelected: boolean = false;
+    public hasWarning: boolean = false;
+    public updatedDate: Date = new Date();
+    public createdDate: Date = new Date();
+
+    constructor(fields: Omit<IBasicConfigurations<TYPE>, OmitInConstructor>) {
+        this.type = fields.type;
+        this.updatedDate = new Date();
+        this.id = fields.id || this.id;
+        this.icon = fields.icon || this.icon;
+        this.ordem = fields.ordem || this.ordem;
+        this.label = fields.label || this.label;
+        this.hasError = fields.hasError || this.hasError;
+        this.isEditing = fields.isEditing || this.isEditing;
+        this.hasWarning = fields.hasWarning || this.hasWarning;
+        this.isExpanded = fields.isExpanded || this.isExpanded;
+        this.isSelected = fields.isSelected || this.isSelected;
+        this.properties = fields.properties || this.properties;
+        this.createdDate = fields.createdDate || this.createdDate;
+        this.description = fields.description || this.description;
+    }
+}
