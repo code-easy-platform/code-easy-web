2f2d5c537cb62d2b8eade73825bb8915017bdfb9
lvargas <lvargas@gvdasa.com.br>
2021-02-22 21:08:51 -0300

    refactor: removed dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,6 @@ import * as serviceWorker from './serviceWorker';
 import { App } from './app/App';
 import './app/style/global.css';
 
-// Inicia o uso a fonte roboto no projeto
-require('typeface-roboto');
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.register();
